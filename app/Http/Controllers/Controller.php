78531be75cbd4f4cd1072c9453bd138abd58ecea
Adebambo Adenuga <adenugaadebambo411@gmail.com>
2021-07-15 11:50:48 +0100

    get browser

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -6,7 +6,6 @@ use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
 use Illuminate\Foundation\Bus\DispatchesJobs;
 use Illuminate\Foundation\Validation\ValidatesRequests;
 use Illuminate\Routing\Controller as BaseController;
-use Illuminate\Http\Request;
 
 use Illuminate\Support\Facades\Mail;
 use App\Mail\sendEmail;
@@ -47,7 +46,9 @@ class Controller extends BaseController
     }
 
     // Get Browser
-    public function myBrowser(Request $request){
+    public function myBrowser(){
+        $request = request();
+
         $browser = get_browser($request->header('User-Agent'), true);
         
         return $browser['browser'];
